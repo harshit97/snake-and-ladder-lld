c9c71d8ffd353da3104e11fd9ac0857960ccf175
harshagarwal <harshagarwal@paypal.com>
2022-02-09 10:08:10 +0530

    Moving snake and ladder details to board class

--- src/hello/lld/snakeladder/model/Board.java
+++ src/hello/lld/snakeladder/model/Board.java
@@ -2,21 +2,36 @@ package hello.lld.snakeladder.model;
 
 import lombok.Value;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Value
 public class Board {
 
     int start;
     int end;
+    Map<Integer, Integer> jumpStartToEndMap;
 
-    private Board(int start, int end) {
+    private Board(int start, int end, Map<Integer, Integer> jumpStartToEndMap) {
         this.start = start;
         this.end = end;
+        this.jumpStartToEndMap = jumpStartToEndMap;
     }
 
-    public Board getNewInstance(int size) {
+    public Board getNewInstance(int size, List<SnakeOrLadder> snakeOrLadderList) {
         if (size <= 0) {
-            throw new IllegalArgumentException("Invalida size value");
+            throw new IllegalArgumentException("Invalid size value");
         }
-        return new Board(1, size);
+        Map<Integer, Integer> snakeOrLadderStartToEndMap = new HashMap<>();
+        for (SnakeOrLadder snakeOrLadder : snakeOrLadderList) {
+            int snakeOrLadderStart = snakeOrLadder.getStart();
+            int snakeOrLadderEnd = snakeOrLadder.getEnd();
+            if (snakeOrLadderStart <= 1 || snakeOrLadderEnd >= size) {
+                throw new IllegalArgumentException("Snake or ladder range should be within board");
+            }
+            snakeOrLadderStartToEndMap.putIfAbsent(snakeOrLadderStart, snakeOrLadderEnd);
+        }
+        return new Board(1, size, snakeOrLadderStartToEndMap);
     }
 }
